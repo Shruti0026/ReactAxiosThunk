4f7ece1e044ac32fbbaf87fe9a3ffa1485046b08
Shruti0026 <naik.shrutika026@gmail.com>
2023-03-09 01:10:56 +0530

    different routes created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Forms from './components/Forms';
+import UpdateDetails from './components/UpdateDetails';
+import { BrowserRouter as Router, Route, Link ,Routes } from 'react-router-dom';
+//import UserData from './components/UserData';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Router>
+          <Routes><Route exact path = '/' element = { <Home /> } ></Route>  
+              <Route exact path = '/Home' element = { <Home /> } ></Route>  
+              <Route exact path='/Forms' element={<Forms />}></Route>
+              <Route exact path='/edit/:id' element={<UpdateDetails />}></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
